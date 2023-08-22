727b22d63090a91146b0e5a63a5bf87ff10531f7
Angelica Gallo <angaar97@gmail.com>
2023-08-22 17:38:45 -0500

    fixed route index

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.use('/api/auth' , require('./routes/auth'));
 app.use('/api/events' , require('./routes/events'));
 
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + './public/index.html');
+})
+
 app.listen( process.env.PORT, () => {
     console.log( `Server running on port ${ process.env.PORT }` );
 });
